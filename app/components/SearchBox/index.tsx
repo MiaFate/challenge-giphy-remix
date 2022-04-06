cfda0552eb783d6ebb0aa82e43bcc5df03a3367d
MiaFate <miapurplefroot@gmail.com>
2022-04-06 16:25:12 -0300

    consumiendo api de giphy

--- app/components/SearchBox/index.tsx
+++ app/components/SearchBox/index.tsx
@@ -6,21 +6,27 @@ interface SearchBoxProps {
     placeholder: string;
 };
 
-
 export default function SearchBox({ placeholder }: SearchBoxProps) {
     const datos = useLoaderData();
+
     const [text, settext] = useState("");
-    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
+
+    const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
         console.log(datos.APIKEY);
-
-
+        const result = await fetchGiPHY(text);
+        console.log(result);
     };
     const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         const { value } = event.target;
         settext(value);
-
     };
+    const fetchGiPHY = async (query:string) => {
+        const response = await fetch(`https://api.giphy.com/v1/gifs/search?api_key=${datos.APIKEY}&q=${query}&limit=25&offset=0&rating=G&lang=en`);
+        const { data } = await response.json();
+        console.log(data);
+    }
+
 
     return (
         <form id="searchbox" onSubmit={handleSubmit}>
